e85ffb3e042e87130686842cb5449826e8f7934d
Karol Piatek <karol.piatek@plasticity.cloud>
2025-01-16 01:30:32 +0000

    Fix for standalone builds.

--- al2023/zlib-ng-testing/zlib-ng-al2023-integration/os-bundle/build_standalone.sh
+++ al2023/zlib-ng-testing/zlib-ng-al2023-integration/os-bundle/build_standalone.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-
+REGISTRY_PATH=localhost
+IMAGE_TAG=al2023
+docker rmi $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-dist
+docker rmi $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-builder
+docker build -f Dockerfile --target zlib-ng-builder --tag $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-builder .
+docker build -f Dockerfile --target zlib-ng-dist --tag $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-dist .
