ebd806cb89179f9b88267fff9126b29a62e5ec1f
Karol Piatek <karol.piatek@plasticity.cloud>
2025-01-16 20:58:21 +0000

    Update to extract release.

--- al2023/zlib-ng-testing/zlib-ng-al2023-integration/os-bundle/build_standalone.sh
+++ al2023/zlib-ng-testing/zlib-ng-al2023-integration/os-bundle/build_standalone.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-REGISTRY_PATH=localhost
-IMAGE_TAG=al2023
+REGISTRY_PATH=ghcr.io
+IMAGE_TAG=plasticity-cloud/al2023-zlib-ng
 docker rmi $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-dist
 docker rmi $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-builder
 docker build -f Dockerfile --target zlib-ng-builder --tag $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-builder .
 docker build -f Dockerfile --target zlib-ng-dist --tag $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-dist .
+
+docker run -b --name=zlib-ng-builder $REGISTRY_PATH/amazonlinux:$IMAGE_TAG-zlib-ng-builder bash
+docker cp zlib-ng-builder:/root/al2023-zlib-ng.tar.gz ./al2023-zlib-ng.tar.gz
+docker stop zlib-ng-builder
+docker rm zlib-ng-builder
