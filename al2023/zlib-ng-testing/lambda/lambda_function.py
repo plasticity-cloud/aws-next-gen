a34713059330a70d277c3c38e05ba26936fd1905
Karol Piatek <karol.piatek@plasticity.cloud>
2025-01-21 23:45:58 +0000

    Fixing execution of lambda function in python.

--- al2023/zlib-ng-testing/lambda/lambda_function.py
+++ al2023/zlib-ng-testing/lambda/lambda_function.py
@@ -5,6 +5,16 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 
 
 def lambda_handler(event: Dict[str, str], context: LambdaContext):
-    s3 = S3Object(bucket=event["bucket"], key=event["key"], is_gzip=True, is_csv=True)
+    s3 = S3Object(bucket=event["bucket"], key=event["key"], is_gzip=True, is_csv=False)
+    dateCounter=0
+    lineCounter=0
     for line in s3:
-        print(line)
+      dateColumn=line[:9]
+      lineCounter += 1
+      print str(dateColumn)
+      if str(dateColumn).startswith("WARC-Date"):
+        dateCounter += 1
+        print(f"Already found {dateCounter}")
+      if lineCounter == 50000
+        break
+    print (f"Number of date objects {dateCounter}")
